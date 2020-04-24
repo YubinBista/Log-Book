cf69a6c3a9df77bccb7ef01838173eab37e156b0
rails course from udemy <lomaslove132@gmail.com>
2020-04-24 22:32:01 +1000

    database setup and connected

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+//connect database
+connectDB();
+
 app.get('/',(req, res)=> 
-res.json({message: 'hello world'}));
+res.json({msg: 'hello world'}));
 
 
 //define routes
 
-app.use('/api/users', require('/routes/users'));
-app.use('/api/auth', require('/routes/auth'));
-app.use('/api/contacts', require('/routes/contacts'))
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT,()=> console.log(
     `Server started on port ${PORT}`
-))
+));
